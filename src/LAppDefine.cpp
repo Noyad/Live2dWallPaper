ecc8f4d9f0df67e1417186d913df8f84204ccd35
Noyad <47940526+Noyad@users.noreply.github.com>
2021-09-11 18:03:56 +0800

    Add files via upload

---
+++ src/LAppDefine.cpp
@@ -0,0 +1,88 @@
+﻿/**
+ * Copyright(c) Live2D Inc. All rights reserved.
+ *
+ * Use of this source code is governed by the Live2D Open Software license
+ * that can be found at https://www.live2d.com/eula/live2d-open-software-license-agreement_en.html.
+ */
+
+#include "LAppDefine.hpp"
+#include <CubismFramework.hpp>
+#include <vector>
+#include <string>
+
+namespace LAppDefine {
+    using namespace std;
+    using namespace Csm;
+
+    // 画面
+    const csmFloat32 ViewMaxScale = 4.0f;
+    const csmFloat32 ViewMinScale = 0.4f;
+
+    const csmFloat32 ViewLogicalLeft = -1.0f;
+    const csmFloat32 ViewLogicalRight = 1.0f;
+
+    const csmFloat32 ViewLogicalMaxLeft = -2.0f;
+    const csmFloat32 ViewLogicalMaxRight = 2.0f;
+    const csmFloat32 ViewLogicalMaxBottom = -2.0f;
+    const csmFloat32 ViewLogicalMaxTop = 2.0f;
+
+    // 相対パス
+    string ResourcesPath = "Resources/";
+
+    // モデルの後ろにある背景の画像ファイル
+    string BackImageName = "back_class_normal.png";
+    // 歯車
+    string GearImageName = "icon_gear.png";
+    // 終了ボタン
+    string PowerImageName = "close.png";
+
+    // モデル定義------------------------------------------
+    // モデルを配置したディレクトリ名の配列
+    // ディレクトリ名とmodel3.jsonの名前を一致させておくこと
+    vector<string> ModelDir = {
+        "edu"
+    };
+    csmInt32 ModelDirSize = ModelDir.size();
+
+    // 外部定義ファイル(json)と合わせる
+    const csmChar* MotionGroupIdle = "Idle"; // アイドリング
+    const csmChar* MotionGroupTapHead = "TouchHead";
+    const csmChar* MotionGroupTapBody = "TouchBody"; // 体をタップしたとき
+    const csmChar* MotionGroupTapSpecial = "TouchSpecial"; // 体をタップしたとき
+
+    // 外部定義ファイル(json)と合わせる
+    const csmChar* HitAreaNameHead = "Head";
+    const csmChar* HitAreaNameBody = "Body";
+    const csmChar* HitAreaNameSpecial = "Special";
+
+    // モーションの優先度定数
+    const csmInt32 PriorityNone = 0;
+    const csmInt32 PriorityIdle = 1;
+    const csmInt32 PriorityNormal = 2;
+    const csmInt32 PriorityForce = 3;
+
+    // デバッグ用ログの表示オプション
+    const csmBool DebugLogEnable = true;
+    const csmBool DebugTouchLogEnable = false;
+
+    // Frameworkから出力するログのレベル設定
+    const CubismFramework::Option::LogLevel CubismLoggingLevel = CubismFramework::Option::LogLevel_Verbose;
+
+    // デフォルトのレンダーターゲットサイズ
+    csmInt32 RenderTargetWidth = 1920;
+    csmInt32 RenderTargetHeight = 1080;
+
+    //Live2dModel显示偏移值
+    csmInt32 RenderTargetBiasX = 0;
+    csmInt32 RenderTargetBiasY = 20;
+
+    //Live2d背景显示偏移值
+    csmInt32 RenderBackgroundBiasX = 0;
+    csmInt32 RenderBackgroundBiasY = 20;
+
+    csmFloat32 Rate = 800;
+
+    csmFloat32 RenderTargetSize = 1;
+
+    csmBool MouseMove = false;
+}
---
+++ src/LAppDefine.hpp
@@ -0,0 +1,80 @@
+﻿/**
+ * Copyright(c) Live2D Inc. All rights reserved.
+ *
+ * Use of this source code is governed by the Live2D Open Software license
+ * that can be found at https://www.live2d.com/eula/live2d-open-software-license-agreement_en.html.
+ */
+#pragma once
+
+#include <CubismFramework.hpp>
+#include <vector>
+#include <string>
+
+/**
+ * @brief  Sample Appで使用する定数
+ *
+ */
+namespace LAppDefine {
+    using namespace std;
+    using namespace Csm;
+
+    extern const csmFloat32 ViewMaxScale;           ///< 拡大縮小率の最大値
+    extern const csmFloat32 ViewMinScale;           ///< 拡大縮小率の最小値
+
+    extern const csmFloat32 ViewLogicalLeft;        ///< 論理的なビュー座標系の左端の値
+    extern const csmFloat32 ViewLogicalRight;       ///< 論理的なビュー座標系の右端の値
+
+    extern const csmFloat32 ViewLogicalMaxLeft;     ///< 論理的なビュー座標系の左端の最大値
+    extern const csmFloat32 ViewLogicalMaxRight;    ///< 論理的なビュー座標系の右端の最大値
+    extern const csmFloat32 ViewLogicalMaxBottom;   ///< 論理的なビュー座標系の下端の最大値
+    extern const csmFloat32 ViewLogicalMaxTop;      ///< 論理的なビュー座標系の上端の最大値
+
+    extern string ResourcesPath;            ///< 素材パス
+    extern string BackImageName;         ///< 背景画像ファイル
+    extern string GearImageName;         ///< 歯車画像ファイル
+    extern string PowerImageName;        ///< 終了ボタン画像ファイル
+
+    // モデル定義--------------------------------------------
+    extern vector<string> ModelDir;               ///< モデルを配置したディレクトリ名の配列. ディレクトリ名とmodel3.jsonの名前を一致させておく.
+    extern csmInt32 ModelDirSize;             ///< モデルディレクトリ配列のサイズ
+
+                                                    // 外部定義ファイル(json)と合わせる
+    extern const csmChar* MotionGroupIdle;          ///< アイドリング時に再生するモーションのリスト
+    extern const csmChar* MotionGroupTapHead;
+    extern const csmChar* MotionGroupTapBody;       ///< 体をタップした時に再生するモーションのリスト
+    extern const csmChar* MotionGroupTapSpecial;
+
+                                                    // 外部定義ファイル(json)と合わせる
+    extern const csmChar* HitAreaNameHead;          ///< 当たり判定の[Head]タグ
+    extern const csmChar* HitAreaNameBody;          ///< 当たり判定の[Body]タグ
+    extern const csmChar* HitAreaNameSpecial;
+
+                                                    // モーションの優先度定数
+    extern const csmInt32 PriorityNone;             ///< モーションの優先度定数: 0
+    extern const csmInt32 PriorityIdle;             ///< モーションの優先度定数: 1
+    extern const csmInt32 PriorityNormal;           ///< モーションの優先度定数: 2
+    extern const csmInt32 PriorityForce;            ///< モーションの優先度定数: 3
+
+                                                    // デバッグ用ログの表示
+    extern const csmBool DebugLogEnable;            ///< デバッグ用ログ表示の有効・無効
+    extern const csmBool DebugTouchLogEnable;       ///< タッチ処理のデバッグ用ログ表示の有効・無効
+
+    // Frameworkから出力するログのレベル設定
+    extern const CubismFramework::Option::LogLevel CubismLoggingLevel;
+
+    // デフォルトのレンダーターゲットサイズ
+    extern csmInt32 RenderTargetWidth;
+    extern csmInt32 RenderTargetHeight;
+
+    extern csmInt32 RenderTargetBiasX;
+    extern csmInt32 RenderTargetBiasY;
+
+    extern csmInt32 RenderBackgroundBiasX;
+    extern csmInt32 RenderBackgroundBiasY;
+
+    extern csmFloat32 Rate;
+
+    extern csmFloat32 RenderTargetSize;
+
+    extern csmBool MouseMove;
+}
